5cc298525ed686207a94dfe1e9ef724033dd24a4
Efstathios Agrapidis <stathisagrapidis@gmail.com>
2013-11-14 17:22:52 +0200

    Print more info to the screen

--- git-sync.sh
+++ git-sync.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "First need to: git remote add upstream https://github.com/otheruser/repo.git"
+echo "First need to: git remote add upstream https://github.com/otheruser/repo.git" &&
+echo "git fetch upstream" &&
 git fetch upstream &&
+echo "git checkout master" &&
 git checkout master &&
+echo "git merge upstream/master" &&
 git merge upstream/master
